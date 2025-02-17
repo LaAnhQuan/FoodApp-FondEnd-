5f95ce9776fa79f3cae129b9946119ff1bd425e8
La Anh Quan <laanhquan1807@gmail.com>
2025-02-17 15:43:11 +0700

    video 28

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.liu}>la anh quan dep trai</Text>
+      </View>
       <Text>Hello world la anh quan </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  liu: {
+    fontSize: 30,
+    color: "red"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
