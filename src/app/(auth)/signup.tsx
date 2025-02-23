c33953aa2c962ea0ac033396614eb5fa93262bed
La Anh Quan <laanhquan1807@gmail.com>
2025-02-23 12:23:07 +0700

    video 78

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,56 +1,60 @@
 import ShareButton from "@/components/button/share.button"
+import SocialButton from "@/components/button/social.button"
+import ShareInput from "@/components/input/share.input"
 import { APP_COLOR } from "@/utils/constant"
+import { Link } from "expo-router"
+import { useState } from "react"
 import { StyleSheet, Text, TextInput, View } from "react-native"
+import { SafeAreaFrameContext, SafeAreaView } from "react-native-safe-area-context"
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 30,
         marginHorizontal: 20,
         gap: 10
     },
-    inputGroup: {
-        padding: 5,
-        gap: 10
 
-    },
-    text: {
-        fontSize: 18,
-
-    },
-    input: {
-        borderColor: "#d0d0d0",
-        borderWidth: 1,
-        paddingHorizontal: 7,
-        paddingVertical: 10,
-        borderRadius: 5,
-
-
-    }
 })
 
 const SignUpPage = () => {
+    const [name, setName] = useState<String>("eric");
+    const [email, setEmail] = useState<String>("");
+    const [password, setPassword] = useState<String>("");
+
     return (
-        <View style={styles.container}>
-            <View>
-                <Text>Đăng ký tài khoản </Text>
-            </View>
-            <View style={styles.inputGroup}>
-                <Text style={styles.text}>Họ Tên</Text>
-                <TextInput style={styles.input} />
-            </View>
-            <View style={styles.inputGroup}>
-                <Text style={styles.text}>Email</Text>
-                <TextInput keyboardType="email-address" style={styles.input} />
-            </View>
-            <View style={styles.inputGroup}>
-                <Text style={styles.text}>Password</Text>
-                <TextInput style={styles.input} />
-            </View>
-            <View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <View>
+                    <Text style={{
+                        fontSize: 25,
+                        fontWeight: 600,
+                        marginVertical: 30
+                    }}>Đăng ký tài khoản </Text>
+                </View>
+                <ShareInput
+                    title="Họ Tên"
+                    value={name}
+                    setValue={setName}
+                />
+                <ShareInput
+                    title="Email"
+                    keyboardType="email-address"
+                    value={email}
+                    setValue={setEmail}
+                />
+                <ShareInput
+                    title="Password"
+                    secureTextEntry={true}
+                    value={password}
+                    setValue={setPassword}
+                />
+
+
+                <View style={{ marginVertical: 5 }}></View>
+
                 <ShareButton
                     title="Đăng ký"
-                    onPress={() => { alert("me") }}
+                    onPress={() => { console.log(name, email, password) }}
                     textStyle={{
                         color: "#fff",
                         paddingVertical: 5,
@@ -65,10 +69,33 @@ const SignUpPage = () => {
 
 
                     }}
-                    pressStyle={{ alignSelf: "stretch" }}
+                    pressStyle={{ alignSelf: "stretch" }} p
                 />
+                <View style={{
+                    marginVertical: 5,
+                    flexDirection: "row",
+                    gap: 10,
+                    justifyContent: "center"
+                }}>
+                    <Text style={{
+                        color: "black"
+                    }}>
+                        Đã có tài khoản?
+                    </Text>
+                    <Link href={"/(auth)/login"}>
+                        <Text style={{
+                            color: "black",
+                            textDecorationLine: 'underline'
+                        }}>
+                            Đăng nhập.
+                        </Text>
+                    </Link>
+                </View>
+                <SocialButton />
             </View>
-        </View>
+
+        </SafeAreaView >
+
     )
 }
 
---
+++ src/components/button/social.button.tsx
@@ -0,0 +1,60 @@
+import { Image, StyleSheet, View } from "react-native"
+import ShareButton from "./share.button"
+import TextBetweenLine from "./text.between.line"
+import fbLogo from '@/assets/auth/facebook.png';
+import ggLogo from '@/assets/auth/google.png'
+
+
+const styles = StyleSheet.create({
+    welcomeBtn: {
+        flex: 1,
+        gap: 20,
+    }
+})
+
+
+const SocialButton = () => {
+    return (
+        <View style={styles.welcomeBtn}>
+            <TextBetweenLine title="Đăng nhập với" />
+
+            <View style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                gap: 30
+            }}>
+                <ShareButton
+                    title="facebook"
+                    onPress={() => { alert("me") }}
+                    textStyle={{ textTransform: "uppercase" }}
+                    btnStyle={{
+                        backgroundColor: "#fff",
+                        justifyContent: "center",
+                        borderRadius: 30
+                    }}
+                    icons={
+                        <Image source={fbLogo} />
+                    }
+                />
+
+                <ShareButton
+                    title="google"
+                    onPress={() => { alert("me") }}
+                    textStyle={{ textTransform: "uppercase" }}
+                    btnStyle={{
+                        backgroundColor: "#fff",
+                        justifyContent: "center",
+                        borderRadius: 30,
+                        paddingHorizontal: 20
+                    }}
+                    icons={
+                        <Image source={ggLogo} />
+                    }
+                />
+            </View>
+        </View>
+
+    )
+}
+
+export default SocialButton
