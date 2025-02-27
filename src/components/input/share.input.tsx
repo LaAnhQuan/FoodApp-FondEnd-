14c876504674f05ff5d06a81aee4646a190b66c1
La Anh Quan <laanhquan1807@gmail.com>
2025-02-27 23:40:32 +0700

    video 98

--- src/components/input/share.input.tsx
+++ src/components/input/share.input.tsx
@@ -34,7 +34,11 @@ interface IProps {
     keyboardType?: KeyboardTypeOptions;
     secureTextEntry?: boolean;
     value?: any;
-    setValue: (v: any) => void;
+    setValue?: (v: any) => void;
+    onChangeText?: any;
+    onBlur?: any;
+    error?: any;
+
 }
 const ShareInput = (props: IProps) => {
 
@@ -42,7 +46,7 @@ const ShareInput = (props: IProps) => {
     const [isShowPassword, setIsShowPassWord] = useState<boolean>(false);
     const {
         title, keyboardType, secureTextEntry = false,
-        value, setValue
+        value, setValue, onChangeText, onBlur, error
     } = props;
     return (
         <View style={styles.inputGroup}>
@@ -50,9 +54,11 @@ const ShareInput = (props: IProps) => {
             <View>
                 <TextInput
                     value={value}
-                    onChangeText={(text) => setValue(text)}
+                    onChangeText={onChangeText}
                     onFocus={() => setIsFocus(true)}
-                    onBlur={() => setIsFocus(false)}
+                    onBlur={(e) => {
+                        onBlur(e); setIsFocus(false);
+                    }}
                     keyboardType={keyboardType}
                     style={[styles.input,
                     {
@@ -60,6 +66,7 @@ const ShareInput = (props: IProps) => {
                     }]}
                     secureTextEntry={secureTextEntry && !isShowPassword}
                 />
+                {error && <Text style={{ color: "red", marginTop: 5 }}>{error}</Text>}
                 {secureTextEntry &&
                     <FontAwesome5
                         style={styles.eye}
--- src/utils/validate.schema.ts
+++ src/utils/validate.schema.ts
@@ -3,8 +3,8 @@ import * as Yup from 'yup';
 export const LoginSchema = Yup.object().shape({
 
     password: Yup.string()
-        .min(2, 'Too Short!')
-        .max(50, 'Too Long!')
+        .min(2, 'Password cần tối thiểu 6 ký tự')
+        .max(50, 'Password tối đa 50 ký tự ')
         .required('Password không được để trống'),
     email: Yup.string()
         .email('Định dạng email không hợp lệ')
