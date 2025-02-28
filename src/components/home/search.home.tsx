ae3aeb17fd48c11587d81be47061c97dacd383b4
La Anh Quan <laanhquan1807@gmail.com>
2025-02-28 23:00:17 +0700

    video 108

--- src/components/home/search.home.tsx
+++ src/components/home/search.home.tsx
@@ -1,11 +1,32 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 import { ZoomIn } from "react-native-reanimated"
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import ShareButton from "../button/share.button";
+import { APP_COLOR } from "@/utils/constant";
 
 
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: APP_COLOR.GREY,
+        gap: 5,
+        flexDirection: "row",
+        margin: 5,
+        paddingHorizontal: 3,
+        paddingVertical: 7,
+        borderRadius: 3
+    }
+})
 const SearchHome = () => {
     return (
-        <View>
-            <Text>Search input</Text>
+        <View style={styles.container}>
+            <EvilIcons
+                name="search"
+                size={20}
+                color="black"
+            />
+            <Text style={{
+                color: "#707070"
+            }}>Deal Hot Hôm Nay Từ 0đ...</Text>
         </View>
     )
 }
