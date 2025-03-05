1cb859b36a6f803dfc9e684dbad6d66fba08140c
La Anh Quan <laanhquan1807@gmail.com>
2025-03-05 22:06:49 +0700

    video 145

--- src/app/(tabs)/account.tsx
+++ src/app/(tabs)/account.tsx
@@ -1,52 +1,150 @@
-import ShareInput from "@/components/input/share.input";
 import { useCurrentApp } from "@/context/app.contex";
-import { Image, Platform, Text, View } from "react-native"
+import { getURLBaseBackEnd } from "@/utils/api";
+import { APP_COLOR } from "@/utils/constant";
+import { View, Text, Image, Pressable, Platform } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Feather from '@expo/vector-icons/Feather';
+
 
 const AccountPage = () => {
-    const { theme, appState } = useCurrentApp();
-
-    const backend = Platform.OS === 'android'
-        ? process.env.EXPO_PUBLIC_ANDROID_API_URL
-        : process.env.EXPO_PUBLIC_IOS_API_URL;
-    const baseImage = `${backend}/images/avatar`;
+    const { appState } = useCurrentApp();
+    const baseImage = `${getURLBaseBackEnd()}/images/avatar`;
+    const insets = useSafeAreaInsets();
 
     return (
-        <View>
-            <View style={{ alignItems: "center", gap: 5 }}>
+        <View style={{ flex: 1 }}>
+            <View style={{
+                paddingTop: insets.top,
+                paddingHorizontal: 20,
+                paddingBottom: 20,
+                backgroundColor: APP_COLOR.ORANGE,
+                flexDirection: "row",
+                gap: 20,
+                alignItems: "center"
+            }}>
                 <Image
-                    style={{ height: 150, width: 150 }}
-                    source={{ uri: `${baseImage}/${appState?.user?.avatar}` }}
+                    style={{ height: 60, width: 60 }}
+                    source={{ uri: `${baseImage}/${appState?.user.avatar}` }}
                 />
-                <Text>{appState?.user.name}</Text>
-            </View>
-            <View style={{ marginTop: 20, gap: 20 }}>
-                <ShareInput
-                    title="Họ Tên"
-                    // onChangeText={handleChange('name')}
-                    // onBlur={handleBlur('name')}
-                    //error={errors.name}
-                    value={appState?.user.name}
-                />
-                <ShareInput
-                    title="Email"
-                    keyboardType="email-address"
-                    // onChangeText={handleChange('email')}
-                    // onBlur={handleBlur('email')}
-                    // error={errors.email}
-                    value={appState?.user.email}
-                />
-                <ShareInput
-                    title="Số điện thoại"
-                    // secureTextEntry={true}
-                    // onChangeText={handleChange('password')}
-                    // onBlur={handleBlur('password')}
-                    // error={errors.password}
-                    value={appState?.user.phone}
-                />
-            </View>
-        </View>
+                <View>
+                    <Text style={{ color: "white", fontSize: 20 }}>
+                        {appState?.user.name}
+                    </Text>
+                </View>
 
+            </View>
+
+            <Pressable style={{
+                paddingVertical: 15,
+                paddingHorizontal: 10,
+                borderBottomColor: "#eee",
+                borderBottomWidth: 1,
+                justifyContent: "space-between",
+                flexDirection: "row",
+                alignItems: "center"
+            }}>
+                <View style={{
+                    flexDirection: "row",
+                    gap: 10,
+                    alignItems: "center"
+                }}>
+                    <Feather name="user-check" size={20} color="green" />
+                    <Text>Cập nhật thông tin</Text>
+                </View>
+
+                <MaterialIcons name="navigate-next" size={24} color="grey" />
+            </Pressable>
+
+            <Pressable style={{
+                paddingVertical: 15,
+                paddingHorizontal: 10,
+                borderBottomColor: "#eee",
+                borderBottomWidth: 1,
+                justifyContent: "space-between",
+                flexDirection: "row",
+                alignItems: "center"
+            }}>
+                <View style={{
+                    flexDirection: "row",
+                    gap: 10,
+                    alignItems: "center"
+                }}>
+                    <MaterialIcons name="password" size={20} color="green" />
+                    <Text>Thay đổi mật khẩu</Text>
+                </View>
+
+                <MaterialIcons name="navigate-next" size={24} color="grey" />
+            </Pressable>
+
+            <Pressable style={{
+                paddingVertical: 15,
+                paddingHorizontal: 10,
+                borderBottomColor: "#eee",
+                borderBottomWidth: 1,
+                justifyContent: "space-between",
+                flexDirection: "row",
+                alignItems: "center"
+            }}>
+                <View style={{
+                    flexDirection: "row",
+                    gap: 10,
+                    alignItems: "center"
+                }}>
+                    <MaterialIcons name="language" size={20} color="green" />
+                    <Text>Ngôn ngữ</Text>
+                </View>
+
+                <MaterialIcons name="navigate-next" size={24} color="grey" />
+            </Pressable>
+
+            <Pressable style={{
+                paddingVertical: 15,
+                paddingHorizontal: 10,
+                borderBottomColor: "#eee",
+                borderBottomWidth: 1,
+                justifyContent: "space-between",
+                flexDirection: "row",
+                alignItems: "center"
+            }}>
+                <View style={{
+                    flexDirection: "row",
+                    gap: 10,
+                    alignItems: "center"
+                }}>
+                    <MaterialIcons name="info-outline" size={20} color="green" />
+                    <Text>Về ứng dụng</Text>
+                </View>
+
+                <MaterialIcons name="navigate-next" size={24} color="grey" />
+            </Pressable>
+
+            <View style={{
+                flex: 1, justifyContent: "flex-end",
+                gap: 10,
+                paddingBottom: 15
+            }}>
+                <Pressable
+                    style={({ pressed }) => ({
+                        opacity: pressed === true ? 0.5 : 1,
+                        padding: 10,
+                        marginHorizontal: 10,
+                        backgroundColor: APP_COLOR.ORANGE,
+                        borderRadius: 3
+                    })}>
+                    <Text style={{
+                        textAlign: "center",
+                        color: "white"
+                    }}>
+                        Đăng Xuất
+                    </Text>
+                </Pressable>
+                <Text style={{ textAlign: "center", color: APP_COLOR.GREY }}>
+                    Version 1.0 - @laanhquan
+                </Text>
+            </View>
+
+        </View>
     )
 }
-
-export default AccountPage
+export default AccountPage;
